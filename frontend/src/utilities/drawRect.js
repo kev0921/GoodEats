7f80d7356702c8993bb2679b16da4a266b018c29
kev0921 <kevin.hu0921@gmail.com>
2024-05-12 15:48:29 -0400

    Added the scanning page with object detection functionality using webcam

---
+++ frontend/src/utilities/drawRect.js
@@ -0,0 +1,26 @@
+export const drawRect = (detections, ctx) => {
+  // Loop through each prediction
+  for (let prediction of detections) {
+
+    // Extract boxes and classes
+    const [x, y, width, height] = prediction['bbox']; 
+    const text = prediction['class']; 
+
+    // Set styling
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    ctx.strokeStyle = '#' + color
+    ctx.font = '18px Arial';
+
+    // Draw rectangles and text
+    ctx.beginPath();   
+    ctx.fillStyle = '#' + color
+    ctx.fillText(text, x, y);
+    ctx.rect(x, y, width, height); 
+    ctx.stroke();
+
+    if (prediction.class == 'cell phone') {
+      console.log('cell phone detected')
+      return prediction.class;
+    }
+  }
+}
